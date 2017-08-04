bf2f063b17ec42b7527e022e458347f589374a05
Morgan Hotonnier <morgan.hotonnier@kindredgroup.com>
2017-08-04 16:15:59 +0200

    Newsletter 7 Aug 2017

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const plugins = gulpLoadPlugins();
 
-gulp.task('html')
-  .src('*.mjml')
-  .pipe(plugins.mjml())
-  .pipe(gulp.dest('./'));
 
+gulp.task('html', function() {
+  return gulp.src('./*.mjml')
+    .pipe(plugins.cached('htmlify'))
+    .pipe(plugins.mjml())
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('default', ['html'], function() {
   gulp.watch(['*.mjml'], ['html']);
